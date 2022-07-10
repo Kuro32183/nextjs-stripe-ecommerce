85cc1855e8a87928370ad0fd87711e7fee698e90
Kuro32183 <takumi32183@gmail.com>
2022-07-11 00:09:04 +0900

    first commit

---
+++ components/ProductCard.tsx
@@ -0,0 +1,57 @@
+import {FunctionComponent, useContext} from "react";
+import Stripe from "stripe";
+import CartContext from "./context/CartContext";
+import {getPriceTotal, getProductDescription, getProductImage, getProductName} from "../utils/computed";
+
+type CardProps = {
+    price: Stripe.Price
+}
+
+const ProductCard: FunctionComponent<CardProps> = ({price}) => {
+    const {add} = useContext(CartContext)
+
+    const addToCart = (p: Stripe.Price) => {
+        if (add) {
+            add(p)
+        }
+    }
+
+    return (
+        <div>
+            <div className="relative mx-4">
+                <div className="relative w-full h-72 rounded-lg overflow-hidden">
+                    <img
+                        src={getProductImage(price.product)}
+                        alt={getProductDescription(price.product)}
+                        className="w-full h-full object-center object-cover"
+                    />
+                </div>
+                <div className="relative mt-4">
+                    <h3 className="text-sm font-medium text-gray-900">{getProductName(price.product)}</h3>
+                    <h4 className="text-sm font-medium text-gray-900">
+                        {getProductDescription(price.product)}
+                    </h4>
+                </div>
+                <div className="absolute top-0 inset-x-0 h-72 rounded-lg p-4 flex items-end justify-end overflow-hidden">
+                    <div
+                        aria-hidden="true"
+                        className="absolute inset-x-0 bottom-0 h-36 bg-gradient-to-t from-black opacity-50"
+                    />
+                    <p className="relative text-lg font-semibold text-white">
+                        ${getPriceTotal(price)}
+                    </p>
+                </div>
+            </div>
+            <div className="mt-6 mx-4">
+                <button
+                    onClick={() => addToCart(price)}
+                    className="relative w-full text-white bg-indigo-600 flex border border-transparent rounded-md py-2 px-8 items-center justify-center text-sm font-medium hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-50 focus:ring-indigo-500"
+                >
+                    Add to Cart<span className="sr-only">, {getProductName(price.product)}</span>
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default ProductCard;
---
+++ pages/index.tsx
@@ -0,0 +1,57 @@
+import type {GetServerSideProps, NextPage} from 'next'
+import {useContext, useEffect} from "react";
+import Stripe from 'stripe';
+import ProductCard from "../components/ProductCard";
+import CartContext from "../components/context/CartContext";
+import Header from "../components/Header";
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+    const stripe = new Stripe(process.env.STRIPE_SECRET ?? '', {
+        apiVersion: '2020-08-27',
+    });
+
+    const res = await stripe.prices.list({
+        limit: 10,
+        expand: ['data.product']
+    });
+
+    const prices = res.data.filter(price => {
+        return price.active;
+    })
+
+    return {
+        props: {
+            prices
+        },
+    }
+}
+
+type Props = {
+    prices: Stripe.Price[]
+}
+
+const Home: NextPage<Props> = ({ prices}) => {
+    return (
+        <main className="bg-gray-100 min-h-screen">
+            <Header/>
+
+            <div className="max-w-5xl mx-auto py-8">
+                <div className="flex items-center justify-between border-b pb-3 pl-4">
+                    <h1 className="font-semibold tracking-wide leading-10 text-xl lg:text-3xl">
+                        
+                    </h1>
+                </div>
+
+                <div className="mt-8 grid grid-cols-1 gap-y-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-3 xl:gap-x-8">
+                    {
+                        prices.map(p => (
+                            <ProductCard key={p.id} price={p}/>
+                        ))
+                    }
+                </div>
+            </div>
+        </main>
+    )
+}
+
+export default Home
